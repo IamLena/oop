54370e76801e022ea2de4561324e33c6a38f179c
Lena <helly.luchina@gmail.com>
2019-08-15 02:13:32 +0300

    vector constructors

--- vector/vector.h
+++ vector/vector.h
@@ -1,22 +1,43 @@
 #pragma once
 #include "base_imp.h"
+#include <iostream>
+#include <initializer_list>
+#include <vector>
+#include <memory>
 
 template <typename  T>
 class Vector : public base::BaseContainer{
 private:
-	std::shared_ptr<T> ptr;
+	std::shared_ptr<T> m_ptr;
 public:
 	Vector();
 	explicit Vector(size_t size);
+	explicit Vector(size_t size, const T* arr);
+	Vector(const std::vector<T> &v);
 	Vector(std::initializer_list<T> list);
-
-	/*Vector(const Vector<T>& another);
-	Vector<T>& operator = (const Vector<T>& another);
+	Vector(const Vector<T>& another);
 	Vector(Vector<T>&& another);
-	Vector<T>& operator = (Vector<T>& another);*/
 
-	base::Iterator<T> begin();
-	base::Iterator<T> end();
+	/*Vector<T>& operator = (const Vector<T>& another);
+	Vector<T>& operator = (Vector<T>& another);
+	Vector<T>& operator = (std::initializer_list<T> list);
+	Vector<T>& operator = (const std::vector<T> v);*/
 
 	~Vector();
+
+	/*base::Iterator<T> begin();
+	base::Iterator<T> end();
+	base::ConstIterator<T> begin() const;
+	base::ConstIterator<T> end() const;*/
+
+	/*
+	scalar_product
+	vector_product
+	coleniar
+	normal
+	angle
+	add + +=
+	minus - -=
+	koef * *=
+	*/
 };
--- vector/vector_imp.h
+++ vector/vector_imp.h
@@ -1,42 +1,63 @@
 #pragma once
 #include "vector.h"
-#include <iostream>
 
 template <typename T>
 Vector<T>::Vector() {
-	ptr = nullptr;
-	std::cout << "created\n";
+	m_ptr = nullptr;
+	this->m_size = 0;
 }
 
 template <typename T>
-Vector<T>::Vector(size_t size) : base::BaseContainer(size) {
-	ptr = std::shared_ptr<T>(new T[size]);
-	std::cout << "created\n";
+Vector<T>::Vector(size_t size) {
+	m_ptr = std::shared_ptr<T>((T*)malloc(size * sizeof(T)), free);
+	this->m_size = size;
 }
 
 template <typename T>
-Vector<T>::Vector(std::initializer_list<T> list) : base::BaseContainer(list.size()) {
-	ptr = std::shared_ptr<T>(new T[list.size()]);
-	T* run = ptr.get();
-	for (auto element : list) {
-		*run = element;
-		run += 1;
+Vector<T>::Vector(size_t size, const T* arr) {
+	this->m_size = size;
+	m_ptr = std::shared_ptr<T>((T*)malloc(size * sizeof(T)), free);
+
+	for (int i = 0; i < size; i++) {
+		m_ptr.get()[i] = arr[i];
 	}
 }
 
 template <typename T>
-Vector<T>::~Vector() {
-	std::cout << "deleted\n";
+Vector<T>::Vector(const std::vector<T> & v) {
+	size_t size = v.size();
+	this->m_size = size;
+	m_ptr = std::shared_ptr<T>((T*)malloc(size * sizeof(T)), free);
+	for (int i = 0; i < size; i++) {
+		m_ptr.get()[i] = v[i];
+	}
+}
+template <typename T>
+Vector<T>::Vector(std::initializer_list<T> list) {
+	size_t size = list.size();
+	this->m_size = size;
+	m_ptr = std::shared_ptr<T>((T*)malloc(size * sizeof(T)), free);
+	int i = 0;
+	for (auto el : list) {
+		m_ptr.get()[i] = el;
+		i++;
+	}
+}
+template <typename T>
+Vector<T>::Vector(const Vector<T>& another) {
+	int size = another.get_size();
+	this->m_size = size;
+	m_ptr = std::shared_ptr<T>((T*)malloc(size * sizeof(T)), free);
+	for (int i = 0; i < size; i++) {
+		m_ptr.get()[i] = another.m_ptr.get()[i];
+	}
 }
 
 template <typename T>
-base::Iterator<T> Vector<T>::begin() {
-	base::Iterator<T> iter(this->get_size(), ptr);
-	return iter;
+Vector<T>::Vector(Vector<T>&& another) {
+	this->m_size = another.get_size();
+	this->m_ptr = another.m_ptr;
 }
+
 template <typename T>
-base::Iterator<T> Vector<T>::end() {
-	base::Iterator<T> iter(this->get_size(), ptr);
-	iter += this->get_size();
-	return iter;
-}
+Vector<T>::~Vector() {};
