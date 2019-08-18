00e846380296d0c1cdcca9feb9c2022b319e0fb4
Lena <helly.luchina@gmail.com>
2019-08-19 00:23:43 +0300

    exceptions

---
+++ base/error.h
@@ -0,0 +1,45 @@
+#pragma once
+#include <exception>
+
+namespace error {
+	class CommonError : public std::exception {
+	public:
+		virtual const char* what() const throw() {
+			return "error";
+		}
+	};
+
+	class ZeroSizeError : public std::exception {
+	public:
+		virtual const char* what() const throw() {
+			return "Zero_Size_Error";
+		}
+	};
+	class SizeDismatch : public std::exception {
+	public:
+		virtual const char* what() const throw() {
+			return "Size_Dismatch";
+		}
+	};
+
+	class MemmoryError : public std::exception {
+	public:
+		virtual const char* what() const throw() {
+			return "Memmory_Error";
+		}
+	};
+
+	class IndexError : public std::exception {
+	public:
+		virtual const char* what() const throw() {
+			return "Index_Error";
+		}
+	};
+
+	class TooMuchDimentions : public std::exception {
+	public:
+		virtual const char* what() const throw() {
+			return "Too_Much_Dimentions";
+		}
+	};
+}
